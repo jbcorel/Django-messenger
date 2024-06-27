c33dbf78357cd823dadcc4e9dfbb0bfe1ca875e3
Max Krivonosov <maxkrivon2003@gmail.com>
2024-06-28 00:30:19 +1000

    moved JS to static

---
+++ messenger/chats/static/chats/js/websockets.js
@@ -0,0 +1,44 @@
+    const roomName = JSON.parse(document.getElementById('json-roomslug').textContent);
+    const userName = JSON.parse(document.getElementById('json-username').textContent);
+
+
+    const chatSocket = new WebSocket(
+        'ws://'
+        + window.location.host
+        + '/ws/'
+        + roomName
+        + '/'
+    );
+
+    chatSocket.onmessage = (e) => {
+        console.log(e)
+
+        const data = JSON.parse(e.data);
+        if (data.message) {
+            let html = '<div class="p-4 bg-gray-200 rounded-xl">';
+                html += '<p class="font-semibold">' + data.username + '</p>';
+                html += '<p>' + data.message + '</p></div>';
+                document.querySelector('#chat-messages').innerHTML += html;
+        } else {
+            alert('Empty message');
+        };
+    };
+
+    chatSocket.onclose = (e) => {
+        console.log('onclose triggered!')
+    };
+
+    document.getElementById('input-btn-send').onclick = (e) => {
+        e.preventDefault();
+        const messageInput = document.getElementById('chat-message-input');
+        const message = messageInput.value;
+
+        chatSocket.send(JSON.stringify ({
+            'message': message,
+            'room': roomName,
+            'username': userName,
+        }));
+
+        messageInput.value = '';
+        return false;
+    }
